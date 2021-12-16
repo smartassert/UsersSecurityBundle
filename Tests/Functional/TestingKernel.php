c341239826e0c55b93c437639cb0bc083b56bec4
Jon Cram <webignition@gmail.com>
2021-12-16 16:06:24 +0000

    Create project skeleton (#2)

---
+++ Tests/Functional/TestingKernel.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\UsersSecurityBundle\Tests\Functional;
+
+use SmartAssert\UsersSecurityBundle\UsersSecurityBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class TestingKernel extends Kernel
+{
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): array
+    {
+        return [
+            new UsersSecurityBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+    }
+}
---
+++ UsersSecurityBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SmartAssert\UsersSecurityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UsersSecurityBundle extends Bundle
+{
+}
