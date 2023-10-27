4251b9a6e216645816c8d2dd7ee6c80d6b36ee09
Jon Cram <webignition@gmail.com>
2023-10-27 14:48:34 +0100

    Use php-cs-fixer 3.35.1 (#75)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -15,4 +15,5 @@ return $config->setRules([
     'php_unit_internal_class' => false,
     'php_unit_test_class_requires_covers' => false,
     'declare_strict_types' => true,
+    'single_line_empty_body' => false,
 ])->setFinder($finder);
